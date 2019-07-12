0f19cc236e4d79e5dda13e4487a82e9ee1d35b0c
Charles Bentley <cbentley@mail.ru>
2019-07-12 18:50:41 +0200

    add parameter source

--- src/pasa/cbentley/swing/cmd/ICommandableRefresh.java
+++ src/pasa/cbentley/swing/cmd/ICommandableRefresh.java
@@ -3,5 +3,9 @@ package pasa.cbentley.swing.cmd;
 public interface ICommandableRefresh {
 
    
-   public void cmdRefresh();
+   /**
+    * The source has new content. Refresh
+    * @param source
+    */
+   public void cmdRefresh(Object source);
 }
