916bb3a8aa81fb6d7d8667dc32c03be17872ead5
Charles Bentley <cbentley@mail.ru>
2019-07-24 20:05:12 +0200

    create MenuBarSwingAbstract

---
+++ src/pasa/cbentley/swing/menu/MenuBarSwingAbstract.java
@@ -0,0 +1,63 @@
+package pasa.cbentley.swing.menu;
+
+import java.awt.event.InputEvent;
+
+import javax.swing.JMenuBar;
+
+import pasa.cbentley.core.src4.ctx.UCtx;
+import pasa.cbentley.core.src4.logging.Dctx;
+import pasa.cbentley.swing.ctx.SwingCtx;
+import pasa.cbentley.swing.imytab.IMyGui;
+import pasa.cbentley.swing.menu.ITechMenu;
+import pasa.cbentley.swing.window.CBentleyFrame;
+
+public abstract class MenuBarSwingAbstract extends JMenuBar implements IMyGui, ITechMenu {
+
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1774778387836051346L;
+
+
+   protected CBentleyFrame   owner;
+
+   protected final SwingCtx  sc;
+
+   public MenuBarSwingAbstract(SwingCtx psc, CBentleyFrame owner) {
+      this.sc = psc;
+      this.owner = owner;
+   }
+
+   public void guiUpdate() {
+      sc.guiUpdateOnChildren(this);
+   }
+
+   //#mdebug
+   public String toString() {
+      return Dctx.toString(this);
+   }
+
+   public void toString(Dctx dc) {
+      dc.root(this, "MenuBarSwingAbstract");
+      toStringPrivate(dc);
+   }
+
+   public String toString1Line() {
+      return Dctx.toString1Line(this);
+   }
+
+   public void toString1Line(Dctx dc) {
+      dc.root1Line(this, "MenuBarSwingAbstract");
+      toStringPrivate(dc);
+   }
+
+   public UCtx toStringGetUCtx() {
+      return sc.getUCtx();
+   }
+
+   private void toStringPrivate(Dctx dc) {
+
+   }
+   //#enddebug
+
+}
