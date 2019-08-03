e4113d6c241664cfb2c41448ca2d2423dc2d7d93
Charles Bentley <cbentley@mail.ru>
2019-08-03 13:39:52 +0200

    PrefIdable def implementation

--- src/pasa/cbentley/swing/interfaces/IStringPrefIDable.java
+++ src/pasa/cbentley/swing/interfaces/IStringPrefIDable.java
@@ -1,10 +1,16 @@
 package pasa.cbentley.swing.interfaces;
+
+import pasa.cbentley.core.src4.logging.IStringable;
+
 /**
- * Provides an class ID selector for getting a preference value
+ * Provides an class ID selector for getting a preference value.
+ * 
+ * Can be implemented by a class or you can use a dummy container for a key with
+ * 
  * @author Charles Bentley
  *
  */
-public interface IStringPrefIDable {
+public interface IStringPrefIDable extends IStringable {
 
    
    public String getSelectorKeyPrefID();
---
+++ src/pasa/cbentley/swing/utils/PrefIdable.java
@@ -0,0 +1,64 @@
+package pasa.cbentley.swing.utils;
+
+import pasa.cbentley.core.src4.ctx.UCtx;
+import pasa.cbentley.core.src4.logging.Dctx;
+import pasa.cbentley.core.src4.logging.IDLog;
+import pasa.cbentley.swing.ctx.SwingCtx;
+import pasa.cbentley.swing.interfaces.IStringPrefIDable;
+
+/**
+ * {@link IStringPrefIDable}
+ * 
+ * @author Charles Bentley
+ *
+ */
+public class PrefIdable implements IStringPrefIDable {
+
+   private String           key;
+
+   protected final SwingCtx sc;
+
+   public PrefIdable(SwingCtx sc, String key) {
+      this.sc = sc;
+      this.key = key;
+
+   }
+
+   public String getSelectorKeyPrefID() {
+      return key;
+   }
+
+   //#mdebug
+   public IDLog toDLog() {
+      return toStringGetUCtx().toDLog();
+   }
+
+   public String toString() {
+      return Dctx.toString(this);
+   }
+
+   public void toString(Dctx dc) {
+      dc.root(this, "IDPanel");
+      toStringPrivate(dc);
+   }
+
+   public String toString1Line() {
+      return Dctx.toString1Line(this);
+   }
+
+   private void toStringPrivate(Dctx dc) {
+
+   }
+
+   public void toString1Line(Dctx dc) {
+      dc.root1Line(this, "IDPanel");
+      toStringPrivate(dc);
+   }
+
+   public UCtx toStringGetUCtx() {
+      return sc.getUCtx();
+   }
+
+   //#enddebug
+
+}
