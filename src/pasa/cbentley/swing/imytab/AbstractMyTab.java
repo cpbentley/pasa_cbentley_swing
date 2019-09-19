1d1180dbd1547e4d18c74b005d8dd110a2a59151
Charles Bentley <cbentley@mail.ru>
2019-09-19 16:14:20 +0200

    uses sc stringbuiler

--- src/pasa/cbentley/swing/imytab/AbstractMyTab.java
+++ src/pasa/cbentley/swing/imytab/AbstractMyTab.java
@@ -4,6 +4,7 @@ import javax.swing.Icon;
 import javax.swing.JPanel;
 
 import pasa.cbentley.core.src4.ctx.UCtx;
+import pasa.cbentley.core.src4.helpers.StringBBuilder;
 import pasa.cbentley.core.src4.logging.Dctx;
 import pasa.cbentley.core.src4.logging.IDLog;
 import pasa.cbentley.core.src4.logging.ITechLvl;
@@ -30,33 +31,15 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
 
    private int               mode;
 
-   protected final SwingCtx  sc;
-
    /**
     * Set by the {@link TabbedBentleyPanel} when the 
     */
    private final TabPosition position;
 
+
+   protected final SwingCtx  sc;
+
    private TabPage           tapPage;
-
-   /**
-    * Returns a different page everytime?
-    * @return
-    */
-   public TabPage getTabPage() {
-      if (tapPage == null) {
-         tapPage = new TabPage(sc, this);
-      }
-      return tapPage;
-   }
-
-   public SwingCtx getSwingCtx() {
-      return sc;
-   }
-   
-   public String getSelectorKeyPrefID() {
-      return interID;
-   }
 
    public AbstractMyTab(SwingCtx sc, String internalID) {
       this.sc = sc;
@@ -68,68 +51,16 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
       return mode;
    }
 
-   public void positionUpdate() {
-   }
-
-   public TabPosition getTabPosition() {
-      return position;
-   }
-
-   public String getTabInternalID() {
-      if (interID == null) {
-         interID = this.getClass().getSimpleName();
-      }
+   public String getSelectorKeyPrefID() {
       return interID;
    }
 
-   public void guiUpdate() {
-      sc.guiUpdateOnChildren(this);
+   public SwingCtx getSwingCtx() {
+      return sc;
    }
 
-   /**
-    * Will be called by {@link AbstractMyTab#initCheck()}
-    * <br>
-    * Users of {@link AbstractMyTab} only have to call {@link AbstractMyTab#initCheck()}
-    * just before displaying it
-    */
-   protected abstract void initTab();
-
-   /**
-    * Called by subclass when tab has been fully initialized
-    */
-   public void initFinished() {
-      isInit = true;
-   }
-
-   public void initCheck() {
-      if (!isInitialized()) {
-         initTab();
-         isInit = true;
-         subTabDidFinishInitCheck();
-      }
-   }
-
-   protected void subTabDidFinishInitCheck() {
-      //#debug
-      toDLog().pFlow(getClass().getSimpleName() + "", this, AbstractMyTab.class, "subTabDidFinishInitCheck", ITechLvl.LVL_05_FINE, true);
-   }
-
-   /**
-    * Returns the Tab tip
-    */
-   public String getTabTip() {
-      return sc.getResString("tab." + interID + ".tip");
-   }
-
-   public Icon getTabIcon(int size, int mode) {
-      return sc.getResIcon(interID, "tab", size, mode);
-   }
-
-   /**
-    * Returns the Tab title.
-    */
-   public String getTabTitle() {
-      return sc.getResString("tab." + interID + ".title");
+   public String getTabExitSound() {
+      return sc.getResSound(interID, "tab", "exit");
    }
 
    /**
@@ -148,6 +79,17 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
       return sc.getResIcon(interID, "tab", size);
    }
 
+   public Icon getTabIcon(int size, int mode) {
+      return sc.getResIcon(interID, "tab", size, mode);
+   }
+
+   public String getTabInternalID() {
+      if (interID == null) {
+         interID = this.getClass().getSimpleName();
+      }
+      return interID;
+   }
+
    /**
     * Path to the sound to be played when the Tab is selected.
     * <br>
@@ -159,16 +101,69 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
       return sc.getResSound(interID, "tab", "intro");
    }
 
-   public String getTabExitSound() {
-      return sc.getResSound(interID, "tab", "exit");
+   /**
+    * Returns a different page everytime?
+    * @return
+    */
+   public TabPage getTabPage() {
+      if (tapPage == null) {
+         tapPage = new TabPage(sc, this);
+      }
+      return tapPage;
    }
+
+   public TabPosition getTabPosition() {
+      return position;
+   }
+
+   /**
+    * Returns the Tab tip
+    */
+   public String getTabTip() {
+      String str = sc.buildStringUISerial("tab.", interID, ".tip");
+      return sc.getResString(str);
+   }
+
+   /**
+    * Returns the Tab title.
+    */
+   public String getTabTitle() {
+      String str = sc.buildStringUISerial("tab.", interID, ".title");
+      return sc.getResString(str);
+   }
+
+   public void guiUpdate() {
+      sc.guiUpdateOnChildren(this);
+   }
+
+   public void initCheck() {
+      if (!isInitialized()) {
+         initTab();
+         isInit = true;
+         subTabDidFinishInitCheck();
+      }
+   }
+
+   /**
+    * Called by subclass when tab has been fully initialized
+    */
+   public void initFinished() {
+      isInit = true;
+   }
+
+   /**
+    * Will be called by {@link AbstractMyTab#initCheck()}
+    * <br>
+    * Users of {@link AbstractMyTab} only have to call {@link AbstractMyTab#initCheck()}
+    * just before displaying it
+    */
+   protected abstract void initTab();
 
    public boolean isInitialized() {
       return isInit;
    }
-   
-   public boolean tabWillBeHiddenByAnotherTab(IMyTab newSelectedTab) {
-      return true;
+
+   public void positionUpdate() {
    }
 
    public void setDisposed() {
@@ -179,17 +174,22 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
       this.mode = mode;
    }
 
-   //#mdebug
-   public String toString() {
-      return Dctx.toString(this);
+   protected void subTabDidFinishInitCheck() {
+      //#debug
+      toDLog().pFlow(getClass().getSimpleName() + "", this, AbstractMyTab.class, "subTabDidFinishInitCheck", ITechLvl.LVL_05_FINE, true);
+   }
+
+   public boolean tabWillBeHiddenByAnotherTab(IMyTab newSelectedTab) {
+      return true;
    }
 
    public IDLog toDLog() {
       return sc.toDLog();
    }
 
-   public String toString1Line() {
-      return Dctx.toString1Line(this);
+   //#mdebug
+   public String toString() {
+      return Dctx.toString(this);
    }
 
    public void toString(Dctx dc) {
@@ -200,6 +200,10 @@ public abstract class AbstractMyTab extends JPanel implements IMyTab, IMyGui, IS
       dc.nlLvlTitleIfNull(position, "TabPosition");
       dc.nlLvlTitleIfNull(tapPage, "TapPage");
       sc.toSD().d(this, dc.nLevel());
+   }
+
+   public String toString1Line() {
+      return Dctx.toString1Line(this);
    }
 
    public void toString1Line(Dctx dc) {
