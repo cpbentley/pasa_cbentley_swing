b66aa3a977e23d6bc0b4be4e786e4910c22946a7
Charles Bentley <cbentley@mail.ru>
2019-10-01 17:33:10 +0200

    fix: stringable

--- src/pasa/cbentley/swing/imytab/FrameReference.java
+++ src/pasa/cbentley/swing/imytab/FrameReference.java
@@ -1,13 +1,17 @@
 package pasa.cbentley.swing.imytab;
 
+import pasa.cbentley.core.src4.ctx.UCtx;
+import pasa.cbentley.core.src4.logging.Dctx;
+import pasa.cbentley.core.src4.logging.IDLog;
+import pasa.cbentley.core.src4.logging.IStringable;
 import pasa.cbentley.swing.ctx.SwingCtx;
 import pasa.cbentley.swing.window.CBentleyFrame;
 
-public class FrameReference {
+public class FrameReference implements IStringable {
 
    protected final SwingCtx sc;
 
-   private CBentleyFrame     frame;
+   private CBentleyFrame    frame;
 
    public FrameReference(SwingCtx sc) {
       this.sc = sc;
@@ -20,5 +24,41 @@ public class FrameReference {
    public void setFrame(CBentleyFrame frame) {
       this.frame = frame;
    }
+
+   
+   //#mdebug
+   public IDLog toDLog() {
+      return toStringGetUCtx().toDLog();
+   }
+
+   public String toString() {
+      return Dctx.toString(this);
+   }
+
+   public void toString(Dctx dc) {
+      dc.root(this, "FrameReference");
+      toStringPrivate(dc);
+   }
+
+   public String toString1Line() {
+      return Dctx.toString1Line(this);
+   }
+
+   private void toStringPrivate(Dctx dc) {
+
+   }
+
+   public void toString1Line(Dctx dc) {
+      dc.root1Line(this, "FrameReference");
+      toStringPrivate(dc);
+   }
+
+   public UCtx toStringGetUCtx() {
+      return sc.getUCtx();
+   }
+
+   //#enddebug
+   
+
 
 }
