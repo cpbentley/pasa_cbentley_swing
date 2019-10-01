1d080b0498abd784411d60a60483d075e2146746
Charles Bentley <cbentley@mail.ru>
2019-10-01 17:32:41 +0200

    events

--- src/pasa/cbentley/swing/ctx/IEventsSwing.java
+++ src/pasa/cbentley/swing/ctx/IEventsSwing.java
@@ -8,16 +8,31 @@ import pasa.cbentley.core.src4.ctx.IEventsCore;
  *
  */
 public interface IEventsSwing extends IEventsCore {
-
-   public static int       PID_02_UI           = 2;
-
-   public static int       EID_02_UI_01_CHANGE = 1;
-
-   public static final int EID_02_UI_ZZ_NUM    = 2;
-
    /**
     * Number of statically defined events
     */
-   int                     BASE_EVENTS         = 4;
+   public static final int BASE_EVENTS               = 4;
+
+   public static final int EID_01_SWING_0_ANY        = 0;
+
+   public static final int EID_01_SWING_ZZ_NUM       = 1;
+
+   public static int       EID_02_UI_00_ANY          = 0;
+
+   public static int       EID_02_UI_01_CHANGE       = 1;
+
+   public static final int EID_02_UI_ZZ_NUM          = 2;
+
+   public static int       EID_03_CMD_0_ANY          = 0;
+
+   public static int       EID_03_CMD_1_STATE_CHANGE = 1;
+
+   public static int       EID_03_CMD_ZZ_NUM         = 2;
+
+   public static int       PID_01_SWING              = 1;
+
+   public static int       PID_02_UI                 = 2;
+
+   public static int       PID_03_CMD                = 3;
 
 }
