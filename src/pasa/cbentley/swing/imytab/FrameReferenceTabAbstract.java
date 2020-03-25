624b68e4c99affccab7fbf8151f81b4763e773cb
Charles Bentley <cbentley@mail.ru>
2020-03-25 14:42:11 +0100

    comments

--- src/pasa/cbentley/swing/imytab/FrameReferenceTabAbstract.java
+++ src/pasa/cbentley/swing/imytab/FrameReferenceTabAbstract.java
@@ -2,12 +2,21 @@ package pasa.cbentley.swing.imytab;
 
 import pasa.cbentley.swing.ctx.SwingCtx;
 
+/**
+ * A {@link FrameReference} that uses {@link FrameIMyTab}
+ * 
+ * @author Charles Bentley
+ *
+ */
 public abstract class FrameReferenceTabAbstract extends FrameReference {
 
    public FrameReferenceTabAbstract(SwingCtx sc) {
       super(sc);
    }
 
+   /**
+    * Custom frame and launch, then shows the frame on screen
+    */
    public void showFrame() {
       if (getFrame() == null) {
          IMyTab tab = createTab();
@@ -22,14 +31,31 @@ public abstract class FrameReferenceTabAbstract extends FrameReference {
       }
    }
 
+   /**
+    * Current {@link FrameIMyTab}
+    * @return
+    */
    public FrameIMyTab getFrameIMyTab() {
-      return (FrameIMyTab)getFrame();
+      return (FrameIMyTab) getFrame();
    }
+
+   /**
+    * Allows the customization of the position and the size
+    * @param f
+    */
    protected void customizeLaunch(FrameIMyTab f) {
       sc.showInNewFrame(f, 0.6f, 0.9f);
    }
 
+   /**
+    * Gives the opportunity of the implementation to set its own icon
+    * @param f
+    */
    protected abstract void customizeFrame(FrameIMyTab f);
 
+   /**
+    * Creates the {@link IMyTab} that will live inside the {@link FrameIMyTab}
+    * @return
+    */
    protected abstract IMyTab createTab();
 }
